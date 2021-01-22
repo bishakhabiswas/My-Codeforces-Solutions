f93bf75948e0ca66a672a46587612a62b6863549
Bishakha Biswas <tandra.bu@gmail.com>
2021-01-23 00:21:51 +0600

    24th commit

--- Solutions_ A/148A.cpp
+++ Solutions_ A/148A.cpp
@@ -7,7 +7,7 @@ int main()
     cin>>k>>l>>m>>n>>d;
     for(int i=1;i<=d;i++)
     {
-        if(i%k==0 || i%l==0 || i%m==0 || i%n==0 )
+        if(i%k==0 || i%l==0 || i%m==0 || i%n==0 ) /// i er je man guloke k,l,m,n er man dara vag korle jodi vagses 0 hoy tobe sei man guloi k,l,m,n er gunitok.gunitok guloi harmed dragon er songkha...
         {
             count++;
         }
