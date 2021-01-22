6116ce809856b93cb81152600c49a08d8bbf1fa7
Bishakha Biswas <tandra.bu@gmail.com>
2021-01-22 23:43:58 +0600

    23th commit

---
+++ Solutions_ A/148A.cpp
@@ -0,0 +1,16 @@
+///148A_Insomnia Cure///
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,l,m,n,d,count=0;
+    cin>>k>>l>>m>>n>>d;
+    for(int i=1;i<=d;i++)
+    {
+        if(i%k==0 || i%l==0 || i%m==0 || i%n==0 )
+        {
+            count++;
+        }
+    }
+    cout<<count;
+}
