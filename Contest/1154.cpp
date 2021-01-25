5f236392d04450eca320a22028dc7053cd5e5271
Bishakha Biswas <tandra.bu@gmail.com>
2021-01-25 23:29:11 +0600

    25th commit

---
+++ Contest/1154.cpp
@@ -0,0 +1,19 @@
+///1154A_Restoring Three Numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[4];
+    int a,b,c;
+    for(int i=0;i<4;i++)
+    {
+        cin>>arr[i];
+    }
+   sort(arr,arr+4);
+   c=arr[3]-arr[0];
+   b=arr[2]-c;
+   a=arr[3]-(b+c);
+   cout<<a<<" "<<b<<" "<<c<<endl;
+
+
+}
