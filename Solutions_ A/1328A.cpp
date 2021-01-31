c4f5a1fca6a870c47e91411aa35a4fbe75429f32
Bishakha Biswas <tandra.bu@gmail.com>
2021-01-31 23:05:14 +0600

    27th commit

---
+++ Solutions_ A/1328A.cpp
@@ -0,0 +1,29 @@
+///1328A_Divisibility Problem
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        long long int a,b;
+        cin>>a>>b;
+        if(a%b==0)
+        {
+            cout<<"0"<<endl;
+        }
+        else{
+            cout<<b-(a%b)<<endl;
+        }
+    }
+
+
+
+}
+
+
+
+
+
