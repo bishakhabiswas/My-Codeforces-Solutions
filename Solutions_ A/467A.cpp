6d69cb667ed1066cd8222680b38b003c3034e45b
Bishakha Biswas <tandra.bu@gmail.com>
2021-02-01 21:45:07 +0600

    28th commit

---
+++ Solutions_ A/467A.cpp
@@ -0,0 +1,23 @@
+///467A_ George and Accommodation
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,p,q,count=0;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>p>>q;
+       if(q-p>=2)
+        {
+        count++;
+       }
+
+
+       }
+      cout<<count<<endl;
+
+
+
+}
