4e384d3cf7d5880b5b2f5dd6a73a7deff7a63d82
Bishakha Biswas <tandra.bu@gmail.com>
2021-02-06 14:23:33 +0600

    30th commit

---
+++ Solutions_ A/451A.cpp
@@ -0,0 +1,27 @@
+///451A.Game With Sticks
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+   int i=1;
+int   count=0;
+   for(;;)
+   {
+       if(n-i==0) break;
+       if(m-i==0) break;
+       i++;
+       count++;
+   }
+   if(count%2==0)
+   {
+       cout<<"Akshat";
+   }
+   else{
+    cout<<"Malvika";
+   }
+
+}
+
