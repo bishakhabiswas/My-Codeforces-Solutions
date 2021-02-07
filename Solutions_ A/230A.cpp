28a7cb4b968177521ab25fe5b6ddca1aa7a221e4
Bishakha Biswas <tandra.bu@gmail.com>
2021-02-07 15:29:42 +0600

    32th commit

---
+++ Solutions_ A/230A.cpp
@@ -0,0 +1,36 @@
+///230A. Dragons
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int s,n,count=0;
+    cin>>s>>n;
+    pair <int,int> a[10000];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i].first>>a[i].second;
+    }
+    sort(a,a+n);
+
+    for(int i=0;i<n;i++)
+    {
+        if(s<=a[i].first)
+        {
+            count++;
+            break;
+        }
+        else{
+            s=s+a[i].second;
+        }
+    }
+    if(count>0)
+    {
+        cout<<"NO"<<endl;
+    }
+
+    else {
+            cout<<"YES"<<endl;
+    }
+}
+
