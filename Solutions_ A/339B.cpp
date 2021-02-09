808e8622a6d07370c5aea801eb83db63e14bd22a
Bishakha Biswas <tandra.bu@gmail.com>
2021-02-10 00:48:42 +0600

    33th commit

---
+++ Solutions_ A/339B.cpp
@@ -0,0 +1,27 @@
+///339B. Xenia and Ringroad
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[m];
+    for(int i=0;i<m;i++)
+    {
+        cin>>arr[i];
+    }
+    long long int count = arr[0]-1;
+    for(int i=0;i<m-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+        {
+            count += n-(arr[i]-arr[i+1]);
+        }
+        else if(arr[i]<arr[i+1])
+        {
+            count += arr[i+1]-arr[i];
+        }
+    }
+    cout<<count;
+}
