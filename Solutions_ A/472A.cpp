d9a44a02d90863f3e2fceb3590665810e4688c3b
Bishakha Biswas <tandra.bu@gmail.com>
2021-02-13 23:52:51 +0600

    33th commit

---
+++ Solutions_ A/472A.cpp
@@ -0,0 +1,17 @@
+///472A. Design Tutorial: Learn from Math
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"4"<<" "<<a-4<<endl;
+
+    }
+    else{
+        cout<<"9"<<" "<<a-9<<endl;
+    }
+}
